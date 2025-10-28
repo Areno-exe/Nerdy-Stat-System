59a8a0ee531a73f215c69294b1a5a089bc3156dc
Areno.exe <87085517+Areno-exe@users.noreply.github.com>
2025-10-28 16:16:45 -0400

    Update Stats.py
    Can now save, load, open and close the start menu

--- Stats.py
+++ Stats.py
@@ -1,29 +1,55 @@
-from ursina import *
-import math
-import random
+from ursina import * 
+import math ; import random ; import pickle ; import os
 app=Ursina('Game Stats')
+window.fullscreen=True
+game_state='menu'
 
-# Fix the Stacking Health Bars, it doesn't seem to line up properly
+# Reopening the menu doesn't hide the stuff behind it. You'll likely need to put all the ui in a master list and disable them all
+# -- UI Elements -- Start Menu --
+
+Start_Menu_Panel=Entity(parent=camera.ui,model='quad',color=color.dark_gray,scale=(2.5, 1),z=-20)
+# -- Menu Buttons --
+Start_Button=Button(parent=Start_Menu_Panel,text='Start Game',color=color.black50,scale=(0.3,0.1),position=(0,.1,-0.1))
+Quit_Button=Button(parent=Start_Menu_Panel,text='Quit Game',color=color.black50,scale=(0.3,0.1),position=(0,-.1,-0.1))
+
+# -- Logic -- Start Menu --
+
+def Start_Game():
+	global game_state
+	game_state='game'
+	Start_Menu_Panel.fade_out(duration=1)
+	Start_Menu_Panel.enabled=False
+	update_game_state()
+
+def Quit_Game():
+	application.quit()
+
+Start_Button.on_click=Start_Game
+Quit_Button.on_click=Quit_Game
+
 # -- Variables -- Implemented -- Health --
 
-Max_Health=30
+Max_Health=493
 Current_Health=1
 Segment_Size=10**math.floor(math.log10(Max_Health))
 Count_of_Health_Bars=math.ceil(Max_Health/Segment_Size)
-Segment_Scaling=1/Count_of_Health_Bars
 Health_Bars=[]
 EXP_Health=0
 NEXT_LEVEL_Health=10+round(math.log(Max_Health),3)
 
 # -- Variables -- Implemented -- Defense (Physical Damage Reduction) --
 
-Max_Defense=150
+Max_Defense=50
 Current_Defense=1
+EXP_Defense=0
+NEXT_LEVEL_Defense=10+round(math.log(Max_Defense),3)
 
 # -- Variables -- Not Implemented -- Strength (Physical Attack) --
 
 Max_Strength=10
-Strength=10
+Current_Strength=10
+EXP_Strength=0
+NEXT_LEVEL_Stregth=10+round(math.log(Max_Strength),3)
 
 # -- Variables -- Not Implemented -- Ideas for later; Order is roughly planned implementation route --
 
@@ -34,25 +60,87 @@ Regeneration=0
 
 # -- UI Elements -- Health Bar --
 
-Health_Bar_bg=Entity(parent=camera.ui,model='quad',color=color.black33,scale=(0.5, 0.04),origin=(-0.5, 0),position=(-0.75,0.48,-0.25))
-Health_Text=Text(parent=Health_Bar_bg,text=f'HP:{Current_Health:.1f}/{Max_Health:.1f}',color=color.white,scale=(2,20),origin=(-0.5,0),always_on_top=True)
+Health_Bar_bg=Entity(parent=camera.ui,model='quad',color=color.black50,scale=(0.7, 0.06),origin=(-0.5, 0),position=(-0.75,0.48,1))
+Health_Text=Text(parent=Health_Bar_bg,text=f'HP:{Current_Health:.1f}/{Max_Health:.1f}',color=color.white,scale=(2,20),origin=(-0.5,0),z=-2)
 Bar_Colors=[color.rgb(196/255, 14/255, 14/255),color.rgb(240/255, 137/255, 2/255),color.rgb(226/255, 237/255, 21/255), # Red, Orange, Yellow
 	color.rgb(30/255, 181/255, 13/255),color.rgb(20/255, 227/255, 250/255), # Green, Turqoise
 	color.rgb(14/255, 26/255, 196/255),color.rgb(106/255, 9/255, 217/255),color.rgb(242/255, 41/255, 112/255), # Blue, Purple, Pink-Red
 	color.rgb(237/255, 83/255, 245/255),color.rgb(209/255, 207/255, 207/255)] # Bubblegum Pink, Dusty White
-EXP_Health_Text=Text(parent=camera.ui,text=f'EXP: {EXP_Health:.1f}/{NEXT_LEVEL_Health:.1f}',origin=(-0.5,0),color=Bar_Colors[3],scale=(1,1),position=(-0.25,0.48))
+EXP_Health_Text=Text(parent=camera.ui,text=f'EXP: {EXP_Health:.1f}/{NEXT_LEVEL_Health:.1f}',origin=(-0.5,0),color=Bar_Colors[3],scale=(1.5,1.75),position=(-0.05,0.48))
 
 # -- UI Elements -- Defense --
 
-Defense_bg=Entity(parent=camera.ui,model='quad',color=color.rgba(12/255,14/255,54/255,83/255),scale=(0.5,0.04),origin=(-0.5,0),position=(-0.75,0.44,-0.25))
-Defense_Text=Text(parent=Defense_bg,text=f'Defense: {round(Current_Defense)}',color=color.white,scale=(2,20),origin=(-0.5,0),always_on_top=True)
-Defense_fg=Entity(parent=Defense_bg,model='quad',color=color.rgb(40/255,47/255,247/255),scale=(1,1),origin=(-0.5,0))
+Defense_bg=Entity(parent=camera.ui,model='quad',color=color.rgba(12/255,14/255,54/255),scale=(0.7,0.06),origin=(-0.5,0),position=(-0.75,0.42,-0.25))
+Defense_Text=Text(parent=Defense_bg,text=f'Defense: {round(Current_Defense)}',color=color.white,scale=(2,20),origin=(-0.5,0),z=-1)
+Defense_fg=Entity(parent=Defense_bg,model='quad',color=color.rgb(40/255,47/255,247/255),scale=(1,1),origin=(-0.5,0),z=-0.26)
+EXP_Defense_Text=Text(parent=camera.ui,text=f'EXP: {EXP_Defense:.1f}/{NEXT_LEVEL_Defense:.1f}',origin=(-0.5,0),color=Bar_Colors[5],scale=(1.5,1.75),position=(-0.05,0.42))
+
+# -- UI Elements -- Strength --
+
+Strength_bg=Entity(parent=camera.ui,model='quad',color='#f7952d',scale=(0.7,0.06),origin=(-0.5,0),position=(-0.75,0.36,-0.25))
+Strength_Text=Text(parent=Strength_bg,text=f'Strength: {round(Current_Strength)}',color=color.white,scale=(2,20),origin=(-0.5,0),z=-1)
 
 # -- Buttons --
 
 Damage_Button=Button(parent=camera.ui,text="You Masochist (;",color=color.red,x=0,y=-0.1,scale=(0.3, 0.1))
 Heal_Button=Button(parent=camera.ui,text='How Boring ):',color=color.green,position=(0,0.1),scale=(0.3,0.1))
 Corrosion_Button=Button(parent=camera.ui,text='More Damage!',color=color.azure,position=(0,0),scale=(0.3,0.1))
+Save_Button=Button(parent=camera.ui,text='Save',color=color.black,scale=(0.2,0.1),position=(-0.1,-0.45))
+Load_Button=Button(parent=camera.ui,text='Load',color=color.black,scale=(0.2,0.1),position=(0.1,-0.45))
+
+# -- Logic -- Game Data ; Saving & Loading --
+
+
+def Get_Data():
+	'''
+	Updates the master list of data for purposes of saving. This is necessary, otherwise it would be loading the initial
+	values from the start of the game
+	'''
+	return {
+	"Max_Health":Max_Health,"Current_Health":Current_Health,"EXP_Health":EXP_Health, # Health Saves
+	"Max_Defense":Max_Defense,"Current_Defense":Current_Defense,"EXP_Defense":EXP_Defense, # Defense Saves
+	"Max_Strength":Max_Strength,"Current_Strength":Current_Strength,"EXP_Strength":EXP_Strength # Strength Saves
+	}
+
+def Save_Data(*args,filename='save.dat'):
+	'''
+	Saves current game state to a file called "save.dat" in the "Saves" subfolder
+	'''
+	data=Get_Data()
+	base_dir=os.path.dirname(__file__)
+	save_dir=os.path.join(base_dir,'saves')
+	os.makedirs(save_dir,exist_ok=True)
+	path=os.path.join(save_dir, filename)
+	with open(path,'wb') as f:
+		pickle.dump(data,f)
+	save_text=Text(parent=camera.ui,text='Game Saved!',origin=(0,0),position=(0,-0.35))
+	destroy(save_text,delay=3)
+
+def Load_Data(filename='save.dat'):
+	'''
+	Loads game data, this is stored in a file called "save.dat" in the "Saves" subfolder
+	'''
+	base_dir=os.path.dirname(__file__)
+	save_dir=os.path.join(base_dir,'saves')
+	path=os.path.join(save_dir, filename)
+	try:
+		with open(path,'rb') as f:
+			loaded_data=pickle.load(f)
+			load_text=Text(parent=camera.ui,text='Game loaded successfully!',origin=(0,0),position=(0,-0.35))
+			destroy(load_text,delay=3)
+			for key,value in loaded_data.items():
+				globals()[key] = value
+			Multi_Health_Update(Current_Health)
+			Defense_Update(Current_Defense)
+			Health_EXP_Update(0)
+			Defense_EXP_Update(0)
+	except FileNotFoundError:
+		load_text=Text(parent=camera.ui,text='Save file not found!',origin=(0,0),position=(0,-0.35))
+		destroy(load_text,delay=3)
+		return None
+
+Save_Button.on_click=Save_Data
+Load_Button.on_click=Load_Data
 
 # -- Logic -- Health --
 
@@ -76,15 +164,17 @@ def Multi_Health_Update(new_health):
 		else:
 			bar.scale_x=0
 		if Current_Health<=0:
-			Defeat_Text=Text(parent=camera.ui,text='You Died ):',color=color.red,scale=8,origin=(0,0))
-			Defeat_Text.fade_out(duration=5)
-			destroy(Defeat_Text,delay=5)
+			Defeat_Text_bg=Entity(parent=camera.ui,model='quad',color=color.black,origin=(0,0),scale=(1,0.5),z=-1)
+			Defeat_Text=Text(parent=camera.ui,text='You Died ):',color=color.red,scale=8,origin=(0,0),always_on_top=True)
+			Defeat_Text.fade_out(duration=5) ; Defeat_Text_bg.fade_out(duration=5)
+			destroy(Defeat_Text,delay=5) ; destroy(Defeat_Text_bg,delay=5)
 
 def Health_EXP_Update(exp):
 	'''
 	Updates Health EXP indicator and the variable based on incoming damage
 	'''
 	global EXP_Health,NEXT_LEVEL_Health,Max_Health
+	exp=math.log(exp+1)
 	EXP_Health+=exp
 	EXP_Health_Text.text=f'EXP: {EXP_Health:.1f}/{NEXT_LEVEL_Health:.1f}'
 	while EXP_Health>=NEXT_LEVEL_Health:
@@ -99,23 +189,25 @@ def Rebuild_Health_Bars():
 	'''
 	Recontructs Health Bars, use if Max Health gets modified at all
 	'''
-	global Segment_Size, Count_of_Health_Bars, Segment_Scaling, Health_Bars
+	global Segment_Size, Count_of_Health_Bars, Health_Bars
 	for bar in Health_Bars:
 		destroy(bar)
 	Health_Bars.clear()
 	Segment_Size=10**math.floor(math.log10(Max_Health))
 	Count_of_Health_Bars=math.ceil(Max_Health/Segment_Size)
-	Segment_Scaling=1/Count_of_Health_Bars	
 	for i in range(Count_of_Health_Bars): # Going to use idea that i=0 is top bar, descending. If Max_Health=400, i=0 is green
 		base_index=(Count_of_Health_Bars-1)-i
 		Color_index=base_index % len(Bar_Colors)
-		bar=Entity(parent=Health_Bar_bg,model='quad',scale=(1,Segment_Scaling),origin=(-0.5,-0.5),position=(0,Segment_Scaling*i-0.5,0.01*i),ignore_depth=True)
+		bar=Entity(parent=Health_Bar_bg,model='quad',scale=(1,1),origin=(-0.5,0),position=(0,0,-1+0.05*i))
 		bar.color=Bar_Colors[Color_index]
 		Health_Bars.append(bar)
 
 # -- Logic -- Defense --
 
 def Damage_via_Defense(Damage):
+	'''
+	Calculates damage taken through defense value; damage dealt will be a fraction of total damage
+	'''
 	global Current_Defense,Current_Health
 	percent_reduction=1-(1/math.cosh(Damage/Current_Defense))
 	final_damage=Damage*(percent_reduction)
@@ -123,12 +215,31 @@ def Damage_via_Defense(Damage):
 	print(f'Blocked {Damage-final_damage1} Damage')
 	Health_EXP_Update(final_damage1)
 	Multi_Health_Update(Current_Health-final_damage1)
+	Defense_EXP_Update(clamp((Damage-final_damage1),0,Current_Health))
 
 def Defense_Update(new_defense):
+	'''
+	Updates defense value after recieving a change
+	'''
 	global Current_Defense
 	Current_Defense=clamp(new_defense,0,Max_Defense)
 	Defense_Text.text=f'Defense: {round(Current_Defense)}'
 	Defense_fg.scale_x=Current_Defense/Max_Defense
+
+def Defense_EXP_Update(exp):
+	'''
+	Updates defense experience and checks if it has leveled up or not ; this DOES NOT update the defense UI
+	'''
+	global Max_Defense,EXP_Defense,NEXT_LEVEL_Defense,Current_Defense
+	exp=math.log10(exp+1)
+	EXP_Defense+=exp
+	EXP_Defense_Text.text=f'EXP: {EXP_Defense:.1f}/{NEXT_LEVEL_Defense:.1f}'
+	while EXP_Defense>=NEXT_LEVEL_Defense:
+		EXP_Defense-=NEXT_LEVEL_Defense
+		Max_Defense+=1
+		NEXT_LEVEL_Defense=10+round(math.log(Max_Defense),3)
+		EXP_Health_Text.text=f'EXP: {EXP_Defense:.1f}/{NEXT_LEVEL_Defense:.1f}'
+		Defense_Update(Current_Defense+1)
 
 # -- Logic -- Damage Button --
 
@@ -141,8 +252,6 @@ def Damage_button_click():
 	print(f"Took {Damage} Damage")
 	Damage_via_Defense(Damage)
 
-Damage_Button.on_click=Damage_button_click
-
 # -- Logic -- Health Button --
 
 def Heal_Button_click():
@@ -153,22 +262,52 @@ def Heal_Button_click():
 	heal=random.randint(40,60)
 	Multi_Health_Update(Current_Health+heal)
 
-Heal_Button.on_click=Heal_Button_click
-
 # -- Logic -- Corrosion Button --
 
 def Corrosion_Button_click():
 	'''
-	Randomly Corrodes Player Defense 1-10 points
+	Randomly Corrodes Player Defense 1-5 points
 	'''
 	global Current_Defense
-	corrosion=random.randint(1,10)
+	corrosion=random.randint(1,5)
 	Defense_Update(Current_Defense-corrosion)
 
-Corrosion_Button.on_click=Corrosion_Button_click
+# -- Logic -- Menu Open & Close --
+
+def update_game_state():
+	'''
+	Prevents buttons from being assigned a function while in the start menu.
+	'''
+	if game_state=='menu':
+		Corrosion_Button.enabled=False
+		Damage_Button.enabled=False
+		Heal_Button.enabled=False
+		pass
+	else:
+		Corrosion_Button.enabled=True
+		Damage_Button.enabled=True
+		Heal_Button.enabled=True
+		
+
+def Open_Menu(key):
+	global game_state
+	if key=='escape':
+		if game_state=='game':
+			game_state='menu'
+			update_game_state()
+			Start_Menu_Panel.enabled=True
+def input(key):
+	Open_Menu(key)
 
 # -- Starting Updates & Run --
+Corrosion_Button.on_click=Corrosion_Button_click
+Damage_Button.on_click=Damage_button_click
+Heal_Button.on_click=Heal_Button_click
+Corrosion_Button.enabled=False
+Damage_Button.enabled=False
+Heal_Button.enabled=False
 Rebuild_Health_Bars()
 Multi_Health_Update(Max_Health)
 Defense_Update(Max_Defense)
+
 app.run()
