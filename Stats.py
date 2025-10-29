330477246614afa0fd82810574a4e36c1d7c7f59
Areno.exe <87085517+Areno-exe@users.noreply.github.com>
2025-10-29 00:50:47 -0400

    Update Stats.py

--- Stats.py
+++ Stats.py
@@ -7,10 +7,10 @@ game_state='menu'
 # Reopening the menu doesn't hide the stuff behind it. You'll likely need to put all the ui in a master list and disable them all
 # -- UI Elements -- Start Menu --
 
-Start_Menu_Panel=Entity(parent=camera.ui,model='quad',color=color.dark_gray,scale=(2.5, 1),z=-20)
+Start_Menu_Panel=Entity(parent=camera.ui,texture='black-white-box',model='quad',scale=(1*window.aspect_ratio,1),z=-20)
 # -- Menu Buttons --
-Start_Button=Button(parent=Start_Menu_Panel,text='Start Game',color=color.black50,scale=(0.3,0.1),position=(0,.1,-0.1))
-Quit_Button=Button(parent=Start_Menu_Panel,text='Quit Game',color=color.black50,scale=(0.3,0.1),position=(0,-.1,-0.1))
+Start_Button=Button(parent=Start_Menu_Panel,texture='game-button-2',text='Start Game',color=color.lime,scale=(0.3,0.1),position=(0,.1,-1))
+Quit_Button=Button(parent=Start_Menu_Panel,texture='game-button-2',text='Quit Game',color=color.lime,scale=(0.3,0.1),position=(0,-.1,-1))
 
 # -- Logic -- Start Menu --
 
@@ -44,12 +44,12 @@ Current_Defense=1
 EXP_Defense=0
 NEXT_LEVEL_Defense=10+round(math.log(Max_Defense),3)
 
-# -- Variables -- Not Implemented -- Strength (Physical Attack) --
+# -- Variables -- Semi-Implemented -- Strength (Physical Attack) --
 
 Max_Strength=10
 Current_Strength=10
 EXP_Strength=0
-NEXT_LEVEL_Stregth=10+round(math.log(Max_Strength),3)
+NEXT_LEVEL_Strength=10+round(math.log(Max_Strength),3)
 
 # -- Variables -- Not Implemented -- Ideas for later; Order is roughly planned implementation route --
 
@@ -60,7 +60,7 @@ Regeneration=0
 
 # -- UI Elements -- Health Bar --
 
-Health_Bar_bg=Entity(parent=camera.ui,model='quad',color=color.black50,scale=(0.7, 0.06),origin=(-0.5, 0),position=(-0.75,0.48,1))
+Health_Bar_bg=Entity(parent=camera.ui,model='quad',color=color.black50,scale=(0.7, 0.06),origin=(-0.5, 0),position=(-0.5*window.aspect_ratio,0.48,1))
 Health_Text=Text(parent=Health_Bar_bg,text=f'HP:{Current_Health:.1f}/{Max_Health:.1f}',color=color.white,scale=(2,20),origin=(-0.5,0),z=-2)
 Bar_Colors=[color.rgb(196/255, 14/255, 14/255),color.rgb(240/255, 137/255, 2/255),color.rgb(226/255, 237/255, 21/255), # Red, Orange, Yellow
 	color.rgb(30/255, 181/255, 13/255),color.rgb(20/255, 227/255, 250/255), # Green, Turqoise
@@ -70,22 +70,27 @@ EXP_Health_Text=Text(parent=Health_Bar_bg,text=f'EXP: {EXP_Health:.1f}/{NEXT_LEV
 
 # -- UI Elements -- Defense --
 
-Defense_bg=Entity(parent=camera.ui,model='quad',color=color.rgba(12/255,14/255,54/255),scale=(0.7,0.06),origin=(-0.5,0),position=(-0.75,0.42,-0.25))
+Defense_bg=Entity(parent=camera.ui,model='quad',color=color.rgba(12/255,14/255,54/255),scale=(0.7,0.06),origin=(-0.5,0),position=(-0.5*window.aspect_ratio,0.42,-0.25))
 Defense_Text=Text(parent=Defense_bg,text=f'Defense: {round(Current_Defense)}',color=color.white,scale=(2,20),origin=(-0.5,0),z=-1)
 Defense_fg=Entity(parent=Defense_bg,model='quad',texture='health-bar-vector',color=color.rgb(40/255,47/255,247/255),scale=(1,1),origin=(-0.5,0),z=-0.26)
 EXP_Defense_Text=Text(parent=Defense_bg,text=f'EXP: {EXP_Defense:.1f}/{NEXT_LEVEL_Defense:.1f}',origin=(-0.5,0),color=Bar_Colors[5],scale=(2,30),position=(1,0))
 
 # -- UI Elements -- Strength --
 
-Strength_bg=Entity(parent=camera.ui,model='quad',color='#f7952d',scale=(0.7,0.06),origin=(-0.5,0),position=(-0.75,0.36,-0.25))
+Strength_bg=Entity(parent=camera.ui,model='quad',color='#964a0c',scale=(0.7,0.06),origin=(-0.5,0),position=(-0.5*window.aspect_ratio,0.36,-0.25))
 Strength_Text=Text(parent=Strength_bg,text=f'Strength: {round(Current_Strength)}',color=color.white,scale=(2,20),origin=(-0.5,0),z=-1)
+Strength_fg=Entity(parent=Strength_bg,model='quad',texture='health-bar-vector',color='#f7952d',z=-0.5,origin=(-0.5,0))
+EXP_Strength_Text=Text(parent=Strength_bg,color=color.hex('#f7952d'),text=f'EXP: {EXP_Strength:.1f}/{NEXT_LEVEL_Strength:.1f}',origin=(-0.5,0),scale=(2,30),position=(1,0))
 
 # -- Buttons --
 
-Lever_Button_Pannel=Button(parent=camera.ui,text='',texture='left-arrow-thumb-box',color=color.orange,origin=(0.5,0.5),position=(0.75,-0.4),scale=(0.05,0.05))
-Damage_Button=Button(parent=camera.ui,text="You Masochist (;",color=color.red,x=0,y=-0.1,scale=(0.3, 0.1))
-Heal_Button=Button(parent=camera.ui,text='How Boring ):',color=color.green,position=(0,0.1),scale=(0.3,0.1))
-Corrosion_Button=Button(parent=camera.ui,text='More Damage!',color=color.azure,position=(0,0),scale=(0.3,0.1))
+Lever_Button_Pannel=Button(parent=camera.ui,text='',texture='left-arrow-thumb-box',color=color.orange,origin=(0.5,0.5),position=(0.5*window.aspect_ratio,-0.4),scale=(0.05,0.05))
+Lever_Button_Pannel_Out=Button(parent=camera.ui,text='',texture='left-arrow-thumb-box',color=color.orange,origin=(0.5,0.5),position=(0.5*window.aspect_ratio-0.4,-0.4),scale=(0.05,0.05))
+Button_Pannel=Entity(parent=camera.ui,model='quad',color=color.black50,origin=(0.5,0),scale=(0.4,1),position=(0.5*window.aspect_ratio,0,-1))
+Damage_Button=Button(parent=Button_Pannel,texture='game-button-2',text="You Masochist (;",color=color.red,origin=(0,0),position=(-0.5,-0.1,-1),scale=(0.9, 0.1))
+Heal_Button=Button(parent=Button_Pannel,texture='game-button-2',text='How Boring ):',color=color.green,origin=(0,0),position=(-0.5,0.1,-1),scale=(0.9,0.1))
+Corrosion_Button=Button(parent=Button_Pannel,texture='game-button-2',text='More Damage!',color=color.azure,origin=(0,0),position=(-0.5,0),scale=(0.9,0.1))
+Weaken_Button=Button(parent=Button_Pannel,texture='game-button-2',text='Weaker Darling',color=color.orange,origin=(0,0),position=(-0.5,-0.2),scale=(0.9,0.1))
 Save_Button=Button(parent=camera.ui,text='Save',color=color.black,scale=(0.2,0.1),position=(-0.105,-0.45))
 Load_Button=Button(parent=camera.ui,text='Load',color=color.black,scale=(0.2,0.1),position=(0.105,-0.45))
 
@@ -231,7 +236,7 @@ def Defense_EXP_Update(exp):
 	'''
 	Updates defense experience and checks if it has leveled up or not ; this DOES NOT update the defense UI
 	'''
-	global Max_Defense,EXP_Defense,NEXT_LEVEL_Defense,Current_Defense
+	global Max_Defense,EXP_Defense,NEXT_LEVEL_Defense,Current_Defense,EXP_Defense_Text
 	exp=math.log10(exp+1)
 	EXP_Defense+=exp
 	EXP_Defense_Text.text=f'EXP: {EXP_Defense:.1f}/{NEXT_LEVEL_Defense:.1f}'
@@ -239,8 +244,49 @@ def Defense_EXP_Update(exp):
 		EXP_Defense-=NEXT_LEVEL_Defense
 		Max_Defense+=1
 		NEXT_LEVEL_Defense=10+round(math.log(Max_Defense),3)
-		EXP_Health_Text.text=f'EXP: {EXP_Defense:.1f}/{NEXT_LEVEL_Defense:.1f}'
+		EXP_Defense_Text.text=f'EXP: {EXP_Defense:.1f}/{NEXT_LEVEL_Defense:.1f}'
 		Defense_Update(Current_Defense+1)
+
+# -- Logic -- Strength --
+
+def Strength_Update(new_strength):
+	'''
+	Updates strength value after recieving a change
+	'''
+	global Current_Strength,Max_Strength, Strength_Text, Strength_fg
+	Current_Strength=clamp(new_strength,0,Max_Strength)
+	Strength_Text.text=f'Strength: {round(Current_Strength)}'
+	Strength_fg.scale_x=Current_Strength/Max_Strength
+
+def Strength_EXP_Update(exp):
+	'''
+	Updates strength experience and checks if it has leveled up or not ; this DOES NOT update the strength UI
+	'''
+	global Max_Strength,EXP_Strength,NEXT_LEVEL_Strength,Current_Strength
+	exp=math.log10(exp+1)
+	EXP_Strength+=exp
+	EXP_Strength_Text.text=f'EXP: {EXP_Strength:.1f}/{NEXT_LEVEL_Strength:.1f}'
+	while EXP_Strength>=NEXT_LEVEL_Strength:
+		EXP_Strength-=NEXT_LEVEL_Strength
+		Max_Strength+=1
+		NEXT_LEVEL_Strength=10+round(math.log(Max_Strength),3)
+		EXP_Strength_Text.text=f'EXP: {EXP_Strength:.1f}/{NEXT_LEVEL_Strength:.1f}'
+		Strength_Update(Current_Strength+1)
+
+# -- Logic -- Button Pannel --
+
+def Open_Button_Pannel():
+	Lever_Button_Pannel.enabled=False
+	Button_Pannel.enabled=True
+	Lever_Button_Pannel_Out.enabled=True
+
+def Close_Button_Pannel():
+	Lever_Button_Pannel.enabled=True
+	Button_Pannel.enabled=False
+	Lever_Button_Pannel_Out.enabled=False
+
+Lever_Button_Pannel.on_click=Open_Button_Pannel
+Lever_Button_Pannel_Out.on_click=Close_Button_Pannel
 
 # -- Logic -- Damage Button & Monsters --
 
@@ -276,21 +322,38 @@ def Corrosion_Button_click():
 	Defense_Update(Current_Defense-corrosion)
 Corrosion_Button.on_click=Corrosion_Button_click
 
+# -- Logic -- Weaken Button --
+
+def Weaken_Button_click():
+	'''
+	Randomly Weakens Player Strength 1-5 points
+	'''
+	global Current_Strength
+	weaken=random.randint(1,5)
+	Strength_Update(Current_Strength-weaken)
+Weaken_Button.on_click=Weaken_Button_click
+
 # -- Logic -- Menu Open & Close --
 
-UI_Master_List=[Health_Bar_bg,Defense_bg,Strength_bg,Save_Button,Load_Button,Corrosion_Button,Damage_Button,Heal_Button]
+Game_UI_Master_List=[Health_Bar_bg,Defense_bg,Strength_bg,Lever_Button_Pannel]
+Menu_UI_Master_List=[Save_Button,Load_Button]
 
 def update_game_state():
 	'''
 	Prevents buttons from being assigned a function while in the start menu.
 	'''
 	if game_state=='menu':
-		for i in UI_Master_List:
+		Close_Button_Pannel()
+		for i in Game_UI_Master_List:
 			i.enabled=False
+		for i in Menu_UI_Master_List:
+			i.enabled=True
 		pass
 	else:
-		for i in UI_Master_List:
+		for i in Game_UI_Master_List:
 			i.enabled=True
+		for i in Menu_UI_Master_List:
+			i.enabled=False
 
 def Open_Menu(key):
 	global game_state
@@ -299,10 +362,13 @@ def Open_Menu(key):
 			game_state='menu'
 			update_game_state()
 			Start_Menu_Panel.enabled=True
+			Start_Button.text='Continue'
 def input(key):
 	Open_Menu(key)
 
 # -- Starting Updates & Run --
+Button_Pannel.enabled=False
+Lever_Button_Pannel_Out.enabled=False
 update_game_state()
 Rebuild_Health_Bars()
 Multi_Health_Update(Max_Health)
